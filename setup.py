45d2ac63680708db139edeb28a992dfc1b7ff310
krystian-jedrzejowski <krystian@cogneto.io>
2015-12-14 15:15:04 +0100

    UPD: Adding file fo pypi

--- setup.py
+++ setup.py
@@ -13,6 +13,7 @@ setup(
     description = 'pointhq.com API client with support for Python 3.x (based on pointhq package)',
     long_description = read('README.md'),
     url = 'https://github.com/cognetoapps/python-pointdns',
+    download_url = 'https://github.com/cognetoapps/python-pointdns/tarball/0.1.1',
     author = 'Cogneto.io',
     author_email = 'accounts@copper.io',
     packages = find_packages(),
